a775173cd5aec49f561556b1a6b7bc2212c3b2af
Shamis Ali <shamis97ali@gmail.com>
2024-11-22 11:35:28 -0700

    Adding MyLists and fixing some existing components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,6 +42,7 @@ function App() {
           <Route path="/movie" element={<Movie />} />
           <Route path="/television" element={<Television />} />
           <Route path="/animation" element={<Animation />} />
+          <Route path="/my-lists" element={<MyLists />} />
         </Routes>
       </Router>
     </AuthProvider>
