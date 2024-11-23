3a7a4ed739a65e77e974a8f7290d6adfd4ec974d
Fatema-Ch <115423993+Fatema-Ch@users.noreply.github.com>
2024-11-22 22:25:42 -0700

    Fatema[20]:created home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,6 +43,7 @@ function App() {
           <Route path="/television" element={<Television />} />
           <Route path="/animation" element={<Animation />} />
           <Route path="/my-lists" element={<MyLists />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </AuthProvider>
