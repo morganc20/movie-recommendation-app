c6da366311593cb8d78a772bc9473133e0a73060
Fatema-Ch <115423993+Fatema-Ch@users.noreply.github.com>
2024-11-22 22:07:46 -0700

    Fatema[10]:creating the main home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,94 @@
-import { useAuth } from '../context/AuthContext';
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
+import TopListsCarousel from '../components/TopListsCarousel';
+import SortSidebar from '../components/SortSidebar';
+import TitleDisplay from '../components/TitleDisplay';
+import Tabs from '../components/Tabs';
+import '../styles/Home.css';
+import Logo from '../assets/logo.svg'; 
 
-const Home = () => {
-  const { logout } = useAuth();
+
+import JawsThumbnail from '../assets/test.jpg';
+import CaptainPhillipsThumbnail from '../assets/test.jpg';
+import AdriftThumbnail from '../assets/test.jpg';
+import ThrillerNightThumbnail from '../assets/test.jpg';
+
+const Animation = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Recommended");
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const categories = ["Recommended", "Weekly 100", "Comedy", "Action", "Romantic", "Thriller", "More"];
+
+  const recommendedMovies = [
+    {
+      id: 1,
+      title: "Jaws",
+      genre: "Thriller",
+      director: "Quentin Tarantino",
+      cast: "abc, def, xyz",
+      movieImage: JawsThumbnail,
+    },
+    {
+      id: 2,
+      title: "Captain Phillips",
+      genre: "Thriller",
+      director: "Quentin Tarantino",
+      cast: "abc, def, xyz",
+      movieImage: CaptainPhillipsThumbnail,
+    },
+    {
+        id: 3,
+        title: "Adrift",
+        genre: "Drama",
+        director: "Quentin Tarantino",
+        cast: "abc, def, xyz",
+        movieImage: AdriftThumbnail,
+    },
+  ];
+
+  const weeklyMovies = [
+    {
+      id: 4,
+      title: "Thriller Night",
+      genre: "Thriller",
+      thumbnail: ThrillerNightThumbnail,
+    },
+  ];
 
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
-      <button onClick={logout}>Logout</button>
+    <div className="forum">
+      <Header />
+      <div className="forum-content">
+      <img src={Logo} alt="Logo" className="main-logo" />
+        <SearchBar />
+          <TopListsCarousel/>
+        <Tabs categories={categories} onSelectCategory={handleCategorySelect} />
+        <div className="main-content">
+          <SortSidebar />
+          <div className="movie-lists">
+            <TitleDisplay
+              title="Jaws"
+              director="Steven Spielberg"
+              genre="Action Adventure"
+              cast="Richard Dreyfuss, Robert Shaw,..."
+              movieImage={recommendedMovies.find((movie) => movie.title === "Jaws")?.movieImage}  // Quick Work: You can call your API endpoint here to retrieve an image and it should be handled
+            />
+            <TitleDisplay
+              title="Jaws"
+              director="Steven Spielberg"
+              genre="Action Adventure"
+              cast="Richard Dreyfuss, Robert Shaw,..."
+              movieImage={recommendedMovies.find((movie) => movie.title === "Jaws")?.movieImage}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default Animation;
